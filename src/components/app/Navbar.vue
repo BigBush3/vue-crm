d268c5aa94cfd2d6bfc19f59d634370c2ff3d8e8
Andrey <andrey.platonov2005@gmail.com>
2021-05-25 16:07:51 +0300

    fully done by me
    it was a good journey

--- src/components/app/Navbar.vue
+++ src/components/app/Navbar.vue
@@ -5,30 +5,30 @@
         <a href="#" @click.prevent="$emit('click')">
           <i class="material-icons black-text">dehaze</i>
         </a>
-        <span class="black-text">12.12.12</span>
+        <span class="black-text">{{date | date('datetime')}}</span>
       </div>
 
       <ul class="right hide-on-small-and-down">
         <li>
           <a
-            class="dropdown-trigger black-text"
-            href="#"
-            data-target="dropdown"
-            ref="dropdown"
+              class="dropdown-trigger black-text"
+              href="#"
+              data-target="dropdown"
+              ref="dropdown"
           >
-            USER NAME
+            {{name}}
             <i class="material-icons right">arrow_drop_down</i>
           </a>
 
-          <ul id="dropdown" class="dropdown-content">
+          <ul id='dropdown' class='dropdown-content'>
             <li>
-              <a href="#" class="black-text">
+              <router-link to="/profile" class="black-text">
                 <i class="material-icons">account_circle</i>Профиль
-              </a>
+              </router-link>
             </li>
             <li class="divider" tabindex="-1"></li>
             <li>
-              <a href="#" class="black-text">
+              <a href="#" class="black-text" @click.prevent="logout">
                 <i class="material-icons">assignment_return</i>Выйти
               </a>
             </li>
@@ -39,15 +39,38 @@
   </nav>
 </template>
 
+
 <script>
 export default {
-  mounted() {
-    M.Dropdown.init(this.$refs.dropdown, {
-      constrainWidth: false,
-    });
+  data: () => ({
+    date: new Date(),
+    interval: null,
+    dropdown: null,
+  }),
+  methods: {
+    async logout() {
+      await this.$store.dispatch('logout')
+      this.$router.push('/login?message=logout')
+    }
   },
-};
+  computed: {
+    name() {
+      return this.$store.getters.info.name
+    }
+  },
+  mounted() {
+    this.interval = setInterval(() => {
+      this.date = new Date()
+    }, 1000)
+    this.dropdown = M.Dropdown.init(this.$refs.dropdown, {
+      constrainWidth: false
+    })
+  },
+  beforeDestroy() {
+    clearInterval(this.interval)
+    if (this.dropdown && this.dropdown.destroy) {
+      this.dropdown.destroy()
+    }
+  }
+}
 </script>
-
-<style>
-</style>
--- src/main.js
+++ src/main.js
@@ -1,14 +1,51 @@
 import Vue from 'vue'
+import Vuelidate from 'vuelidate'
+import Paginate from 'vuejs-paginate'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import dateFilter from '@/filters/date.filter'
+import currencyFilter from '@/filters/currency.filter'
+import localizeFilter from '@/filters/localize.filter'
+import tooltipDirective from '@/directives/tooltip.directive'
+import messagePlugin from '@/utils/message.plugin'
+import Loader from '@/components/app/Loader'
 import './registerServiceWorker'
 import 'materialize-css/dist/js/materialize.min'
 
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/database'
+
 Vue.config.productionTip = false
 
-new Vue({
-	router,
-	store,
-	render: h => h(App)
-}).$mount('#app')
+Vue.use(messagePlugin)
+Vue.use(Vuelidate)
+Vue.filter('date', dateFilter)
+Vue.filter('localize', localizeFilter)
+Vue.filter('currency', currencyFilter)
+Vue.directive('tooltip', tooltipDirective)
+Vue.component('Loader', Loader)
+Vue.component('Paginate', Paginate)
+
+firebase.initializeApp({
+	apiKey: "AIzaSyDS-hZPcmSgNUkQhPioysg5kb78ZcQfN6s",
+	authDomain: "vue-crm-a5576.firebaseapp.com",
+	projectId: "vue-crm-a5576",
+	storageBucket: "vue-crm-a5576.appspot.com",
+	messagingSenderId: "399776963217",
+	appId: "1:399776963217:web:cdcf4cdaf1d2ea2b8d7d30",
+	measurementId: "G-NC6JW48B34"
+})
+
+let app
+
+firebase.auth().onAuthStateChanged(() => {
+	if (!app) {
+		app = new Vue({
+			router,
+			store,
+			render: h => h(App)
+		}).$mount('#app')
+	}
+})
